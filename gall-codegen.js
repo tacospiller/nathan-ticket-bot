1124cc2d00867d278e896805eab76410cef0ab6c
tacospiller <mj3428@gmail.com>
2017-04-03 18:12:04 +0900

    Modify gall reply (now works sometimes)
    The site uses internal javascript to generate service_code.
    First number of _r must be modified properly, could not find the
    modification method yet.

---
+++ gall-codegen.js
@@ -0,0 +1,66 @@
+ function keyGen(ip) {
+    var k = "qAr0Bs1Ct2D3uE4Fv5G6wH7I8xJ9K+yL/M=zNaObPcQdReSfTgUhViWjXkYlZmnop";
+    var o = "";
+    var c1, c2, c3;
+    var e1, e2, e3, e4;
+    var i = 0;
+    ip = ip.replace(/[^A-Za-z0-9+/=]/g, "");
+    while (i < ip.length) {
+        e1 = k.indexOf(ip.charAt(i++));
+        e2 = k.indexOf(ip.charAt(i++));
+        e3 = k.indexOf(ip.charAt(i++));
+        e4 = k.indexOf(ip.charAt(i++));
+        c1 = (e1 << 2) | (e2 >> 4);
+        c2 = ((e2 & 15) << 4) | (e3 >> 2);
+        c3 = ((e3 & 3) << 6) | e4;
+        o = o + String.fromCharCode(c1);
+        if (e3 != 64) {
+            o = o + String.fromCharCode(c2);
+        }
+        if (e4 != 64) {
+            o = o + String.fromCharCode(c3);
+        }
+    }
+    return o;
+}
+
+
+module.exports = {
+
+    keyFinder: (body) => {
+    if (body.includes('var _r =')) {
+        console.log('okay');
+    }
+    const match = RegExp(/var \_r \= \_d\(\'([^\']+)\'\)/).exec(body);
+    console.log(match[0] + '\r\n' + match[1]);
+    return match[1];
+ },
+
+ serviceCode: (k, origCode) => {
+     console.log('serviceCode ' + k + '\r\n' + origCode + '\r\n');
+    let _r = keyGen(k);
+    let _rs;
+
+    if (typeof (_r) == 'string')
+        _rs = _r.split(',');
+    var rc = '';
+
+    const digits = parseInt(_rs[0]) % 100;
+    if (digits <= 43 || digits >= 89) {
+        _rs[0] = 300 + digits;
+    } else {
+        _rs[0] = 600 + digits;
+    }
+
+    console.log(_rs);
+
+    for (var i = 0; i < _rs.length; i++) {
+        rc += String.fromCharCode(((_rs[i] - i - 1) * 2) / (13 - i - 1));
+    }
+    const res = origCode.replace(/(.{10})$/, rc);
+    console.log('result: ' + res);
+    return res;
+}
+}
+
+//module.exports.serviceCode('40qg30wVE=Xi30wTuUTUuzNRuzqk30BjEUXi30BiEUTUE6/Ru6qi30NU3zwp', '21ac6e96ad152e8f15a05b7350a24759b5606fa191c17e042e4d0175735f4d687feb86f6d050ef850165ede88a2bf08f0e879222c2df6c62ef846ae2776983077c991b9fbf1a964eaa5a87b142e36bc80dec22c1590e50d22088a9d2613195e881064c811d126f213f506217b5f5a910f321bf2dab731615239d07248d48b13358b0ab890675e589764bdbe6daedae5274d60412b15ce1bc6ce0fcff7341057901909ff1aea3882d');
--- gall-crawler.js
+++ gall-crawler.js
@@ -8,10 +8,104 @@ const utils = require('./utils');
 const CONFIG = require('./config');
 const querystring = require('querystring');
 const cookieClient = require('cookie-client');
+const codeGen = require('./gall-codegen');
 
 const http = require('http');
 
 let LAST_POST = 0;
+
+function sendReply($, cookieJar, agent, message, url, body, next) {
+    const requestBody = {};
+    
+    $('form#comment_write input[type="hidden"]').each((i, e) => {
+        requestBody[$(e).attr('name')] = $(e).val();
+    });
+
+    requestBody['name'] = 'ㅇㅇ';
+    requestBody['password'] = randomWords();
+    requestBody['memo'] = message;
+
+    requestBody['cur_t'] = $("#cur_t").val();
+    requestBody['id'] = $("#id").val();
+    requestBody['no'] = $("#no").val();
+    requestBody['best'] = 'undefined';
+    requestBody['best_pno'] = 'undefined';
+    requestBody['best_origin'] = '';
+    requestBody['check_6'] = $('#check_6').val();
+    requestBody['check_7'] = $('#check_7').val();
+    requestBody['check_8'] = $('#check_8').val();
+    requestBody['campus'] = '0';
+    requestBody['recommend'] = '0';
+    requestBody['code'] = 'undefined';
+    requestBody['user_ip'] = $('#user_ip').val();
+
+    const codeKey = codeGen.keyFinder(body);
+    requestBody['service_code'] = codeGen.serviceCode(codeKey, requestBody['service_code']);
+
+//    requestBody['service_code'] = codeGen.serviceCode('40vh30tk4GTiu0/Ruzuh3zwRuztg30Bk4rXi30BWurTgE0/RuzBg30/i', '21ac6e96ad152e8f15a05b7350a24759b5606fa191c17e042e4d0175735f4d687feb82f0da55ef850165ede88a2bf08f5d1d766e9e153e9ed48034a7ee6c9f58bc824d59fe156df68168cc79a6d2f5f14ac7622c9ba9ee8e19b489a8fa36bb9d157f66eb4f62da98f77d5f35ebea663ab6e53d0d0f0a0f0c253458bad06af8280273c72a898c7e80d38141860b7211d829548a4fc64313ed93ea73a546df6f559f0f089fe16738e2');
+//    requestBody['ci_t'] = 'cbb563e57106423984d9868759261fd9';
+    console.log(JSON.stringify(requestBody));
+
+    let cookieString = cookieJar.cookieStringForRequest(CONFIG.get('gallHost'), '/');
+    cookieString += '; siteUniqId=STU_58993e00Zlq2Czus; notice_no=23144; lately_cookie=theaterM%7C%uC5F0%uADF9%2C%20%uBBA4%uC9C0%uCEEC%7C8@@lunatichai%7C%uB8E8%uB098%uD2F1%uD558%uC774%7C21@@pokemongo%7C%uD3EC%uCF13%uBAAC%20GO%7C18@@hit%7CHIT%7C1@@alcohol%7C%uC8FC%uB958%7C7';
+
+    const payload = querystring.stringify(requestBody);
+    const options = {
+        host: 'gall.dcinside.com',
+        path: '/forms/comment_submit',
+        method: 'POST',
+        headers: {
+            'Content-Length': Buffer.byteLength(payload),
+            'Accept': '*/*',
+            'Origin': 'http://gall.dcinside.com',
+            'X-Requested-With': 'XMLHttpRequest',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Referer': url,
+            //'Accept-Encoding': 'gzip, deflate',
+            //'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4',
+            'Cookie': cookieString,
+        },
+        agent: agent,
+    }
+    console.log(options.headers.Cookie);
+    console.log(payload);
+    let resp = '';
+    const req = http.request(options, (response) => {
+        response.on('data', (d) => {
+            resp += d;
+        });
+
+        response.on('end', () => {
+            console.log(resp);
+            if (next.length > 0) {
+                next[0]($, cookieJar, agent, next.slice(1));
+            }
+        });
+    });
+
+    req.write(payload,
+    'UTF-8');
+    req.end();
+
+/*
+    request(options, (error, response, body) => {
+        if (error) {
+            console.log(error);
+            return;
+        }
+        if (!parseInt(body, 10)) {
+        //     console.log(body);
+        //    console.log(JSON.stringify(requestBody));
+            //   console.log(querystring.stringify(requestBody));
+            telegram.sendMessage(chatId, '문제가 있는 것 같은데. 패스워드는 ' + requestBody.password + '이야.' + body, undefined, origMessage);
+            return;
+        }
+        console.log(body);
+        console.log(JSON.stringify(requestBody));
+        telegram.sendMessage(chatId, '댓글을 잘 보냈어. 패스워드는 ' + requestBody.password + '이야.', undefined, origMessage);
+    })*/
+}
 
 module.exports = {
     init: () => {
@@ -84,86 +178,7 @@ module.exports = {
             });
             
             let $ = cheerio.load(body);
-            const requestBody = {};
-            $('form#comment_write input[type="hidden"]').each((i, e) => {
-                requestBody[$(e).attr('name')] = $(e).val();
-            });
-
-            requestBody['name'] = 'ㅇㅇ';
-            requestBody['password'] = randomWords();
-            requestBody['memo'] = message;
-
-            requestBody['cur_t'] = $("#cur_t").val();
-            requestBody['id'] = $("#id").val();
-            requestBody['no'] = $("#no").val();
-            requestBody['best'] = 'undefined';
-            requestBody['best_pno'] = 'undefined';
-            requestBody['best_origin'] = '';
-            requestBody['check_6'] = $('#check_6').val();
-            requestBody['check_7'] = $('#check_7').val();
-            requestBody['check_8'] = $('#check_8').val();
-            requestBody['campus'] = '0';
-            requestBody['recommend'] = '0';
-            requestBody['code'] = 'undefined';
-            requestBody['user_ip'] = $('#user_ip').val();
-
-            let cookieString = 'siteUniqId=STU_58d6793cpspvTMwN; ' 
-                + cookieJar.cookieStringForRequest(CONFIG.get('gallHost'), '/forms/comment_submit', false);
-            cookieString += '; __utmt=1; lately_cookie=theaterM%7C%uC5F0%uADF9%2C%20%uBBA4%uC9C0%uCEEC%7C8; __utma=118540316.1840529439.1490450732.1491148807.1491151122.5; __utmb=118540316.6.10.1491151122; __utmc=118540316; __utmz=118540316.1490542821.3.3.utmcsr=gall.dcinside.com|utmccn=(referral)|utmcmd=referral|utmcct=/theaterM/2025357';
-
-            const payload = querystring.stringify(requestBody);
-            const options = {
-                host: 'gall.dcinside.com',
-                path: '/forms/comment_submit',
-                method: 'POST',
-                headers: {
-                    'Content-Length': Buffer.byteLength(payload),
-                    'Accept': '*/*',
-                    'Origin': 'http://gall.dcinside.com',
-                    'X-Requested-With': 'XMLHttpRequest',
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
-                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                    'Referer': url,
-                    //'Accept-Encoding': 'gzip, deflate',
-                    //'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4',
-                    'Cookie': cookieString,
-                },
-                agent: agent,
-            }
-            console.log(options.headers.Cookie);
-            console.log(payload);
-            let resp = '';
-            const req = http.request(options, (response) => {
-                response.on('data', (d) => {
-                    resp += d;
-                });
-
-                response.on('end', () => {
-                    console.log(resp);
-                })
-            });
-
-            req.write(payload,
-            'UTF-8');
-            req.end();
-
-/*
-            request(options, (error, response, body) => {
-                if (error) {
-                    console.log(error);
-                    return;
-                }
-                if (!parseInt(body, 10)) {
-               //     console.log(body);
-                //    console.log(JSON.stringify(requestBody));
-                 //   console.log(querystring.stringify(requestBody));
-                    telegram.sendMessage(chatId, '문제가 있는 것 같은데. 패스워드는 ' + requestBody.password + '이야.' + body, undefined, origMessage);
-                    return;
-                }
-                console.log(body);
-                console.log(JSON.stringify(requestBody));
-                telegram.sendMessage(chatId, '댓글을 잘 보냈어. 패스워드는 ' + requestBody.password + '이야.', undefined, origMessage);
-            })*/
+            sendReply($, cookieJar, agent, message, url, body, []);
         })
     },
 
